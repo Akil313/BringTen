35b218a51f248e2d1e2090c838666822c6a8f42e
Akil313 <akilhosang@gmail.com>
2024-12-17 00:46:34 -0400

    create card and deck struct

--- main.go
+++ main.go
@@ -11,15 +11,49 @@ import (
 	"os"
 )
 
+type card struct {
+	suit  string
+	value string
+}
+
+type deck struct {
+	cards []card
+}
+
+func newDeck() *deck {
+	suits := []string{"C", "H", "S", "D"}
+	values := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+
+	cards := []card{}
+	for _, v := range values {
+		for _, s := range suits {
+			cards = append(cards, card{value: v, suit: s})
+		}
+	}
+
+	return &deck{cards: cards}
+}
+
 func greetPlayer(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("player has joined")
 	io.WriteString(w, "Welcome to BringTen!\n")
+}
+
+func createDeck(w http.ResponseWriter, r *http.Request) {
+
+	tempDeck := newDeck()
+
+	fmt.Printf("new deck made")
+	io.WriteString(w, fmt.Sprintf("%+v\n", tempDeck))
 }
 
 func main() {
 
 	mux := http.NewServeMux() // Routes request to approriate handler
 	mux.HandleFunc("/", greetPlayer)
+	mux.HandleFunc("/deck", createDeck)
+
+	fmt.Println("Server is up!")
 
 	err := http.ListenAndServe(":8080", mux)
 
