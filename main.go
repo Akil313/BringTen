2e4e64255854e3a0127eb0caede5e7f142b9076e
Akil313 <akilhosang@gmail.com>
2024-12-26 03:18:50 -0400

    create and join room handlers

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
-//go:build ignore
-
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
-	// "log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 type card struct {
@@ -18,6 +18,27 @@ type card struct {
 
 type deck struct {
 	cards []card
+}
+
+// Fisher-Yates shuffle
+func (d *deck) shuffle() error {
+
+	if len(d.cards) != 52 {
+		return errors.New("deck is not properly filled\n")
+	}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	shufIndex := r.Perm(len(d.cards))
+
+	newDeck := make([]card, 52)
+
+	for i, v := range shufIndex {
+		newDeck[i] = d.cards[v]
+	}
+
+	d.cards = newDeck
+
+	return nil
 }
 
 func newDeck() *deck {
@@ -34,24 +55,92 @@ func newDeck() *deck {
 	return &deck{cards: cards}
 }
 
+type room struct {
+	id      string
+	name    string
+	canJoin bool
+	deck    deck
+}
+
+type roomManager struct {
+	rooms map[string]*room
+}
+
+func (rm *roomManager) generateRoomId(length int) (string, error) {
+
+	if length < 4 {
+		return "", errors.New("Length of room id must be more than 3 characters")
+	}
+
+	charset := "1234567890abcdefghijklmnopqrstuvwxyz"
+	roomId := make([]byte, length)
+
+	for i := 0; i < length; i++ {
+		roomId[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(roomId), nil
+}
+
+func (rm *roomManager) addNewRoom(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Printf("Method is: %v\n", r.Method)
+	roomId, _ := rm.generateRoomId(4)
+
+	defer r.Body.Close()
+	var body struct {
+		RoomName string `json:"room_name"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+	roomName := body.RoomName
+
+	rm.rooms[roomId] = &room{id: roomId, name: roomName, canJoin: true}
+
+	io.WriteString(w, fmt.Sprintf("Room {%v} has been created with ID: %v\n", body.RoomName, roomId))
+	fmt.Printf("room{%v} created\n", roomId)
+}
+
+func (rm *roomManager) joinRoom(w http.ResponseWriter, r *http.Request) {
+
+	roomId := r.PathValue("id")
+	fmt.Printf(roomId)
+	roomName := rm.rooms[roomId].name
+	io.WriteString(w, fmt.Sprintf("You have joined {%v} with ID: {%v}\n", roomName, roomId))
+	fmt.Printf("player joined room {%v}\n", roomId)
+}
+
 func greetPlayer(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("player has joined")
+	fmt.Printf("player has joined\n")
 	io.WriteString(w, "Welcome to BringTen!\n")
 }
 
 func createDeck(w http.ResponseWriter, r *http.Request) {
 
 	tempDeck := newDeck()
+	err := tempDeck.shuffle()
 
-	fmt.Printf("new deck made")
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
 	io.WriteString(w, fmt.Sprintf("%+v\n", tempDeck))
 }
 
 func main() {
 
+	roomManager := &roomManager{
+		rooms: make(map[string]*room),
+	}
+
 	mux := http.NewServeMux() // Routes request to approriate handler
 	mux.HandleFunc("/", greetPlayer)
 	mux.HandleFunc("/deck", createDeck)
+	mux.HandleFunc("POST /room/{id}/join", roomManager.joinRoom)
+	mux.HandleFunc("POST /room", roomManager.addNewRoom)
 
 	fmt.Println("Server is up!")
 
